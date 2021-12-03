c775f6f469c105a6a257fd52f74033fbd890ead7
Pruthviml <pruthvi.ml@signavio.com>
2021-12-03 15:18:10 +0100

    add testcase for default seedlers

--- src/test/java/com/sunrise/seedling/SeedlingApplicationTests.java
+++ src/test/java/com/sunrise/seedling/SeedlingApplicationTests.java
@@ -1,13 +1,24 @@
 package com.sunrise.seedling;
 
+import com.sunrise.seedling.model.Seedler;
+import com.sunrise.seedling.service.SeedlerService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class SeedlingApplicationTests {
 
-//	@Test
-//	void contextLoads() {
-//	}
+    @Test
+    void testDefaultSeedlers_SeedlerDetails()
+    {
+       List<Seedler> seedlers = SeedlerService.defaultSeedlers();
+
+        Assertions.assertNotNull(seedlers);
+        Assertions.assertTrue(seedlers.size() > 0);
+
+    }
 
 }
